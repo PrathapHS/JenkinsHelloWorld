245f637f5729d79660c28d813eb67420ea9efac0
PrathapHS <113244606+PrathapHS@users.noreply.github.com>
2025-07-28 13:14:26 +0530

    Update HelloWorld.java SCRUM-8

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
 public static void main(String args[]){
 
-System.out.println("Hello world and welcome to Jenkins trainings***");
+System.out.println("Hello world and welcome to   Jenkins trainings***");
 }
 
 }
